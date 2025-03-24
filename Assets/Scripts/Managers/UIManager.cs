85618ad2fcb432bd0fdf603445b0c0cb55669eae
KSangMin <fhfpszmfnwm@naver.com>
2025-03-24 17:44:53 +0900

    [Feat] 기본 UI 캔버스 생성

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -12,7 +12,7 @@ public class UIManager : Singleton<UIManager>
 
     Transform _root;
     Transform Root
-    {
+    {//UI의 부모가 되는 루트 트랜스폼을 반환하며, 없으면 생성합니다.
         get
         {
             if(_root == null)
@@ -24,12 +24,11 @@ public class UIManager : Singleton<UIManager>
     }
 
     public T GetUI<T>() where T : UI
-    {
+    {//UI 매니저에 등록된 특정 타입의 UI를 찾아 반환합니다.
         Type uiType = typeof(T);
 
         if (_sceneDict.TryGetValue(uiType, out UI existingUI))
         {
-            existingUI.Hide();
             return existingUI as T;
         }
 
@@ -37,7 +36,7 @@ public class UIManager : Singleton<UIManager>
     }
 
     public T ShowPopupUI<T>() where T : UI_Popup
-    {
+    {//팝업 UI를 인스턴스화하고 표시한 후, 팝업 스택에 추가
         Type uiType = typeof(T);
 
         T ui = Util.InstantiatePrefabAndGetComponent<T>(path: $"UI/{uiType.Name}", parent: Root);
@@ -48,7 +47,7 @@ public class UIManager : Singleton<UIManager>
     }
 
     public void RemovePopupUI()
-    {
+    {//팝업 스택에서 최상위 팝업 UI를 제거하고 해당 게임 오브젝트를 파괴.
         if (_popupUIs.Count <= 0) return;
 
         UI_Popup popup = _popupUIs.Pop();
@@ -58,7 +57,7 @@ public class UIManager : Singleton<UIManager>
     }
 
     public T HideUI<T>() where T : UI
-    {
+    {//UI SetActive(false); 없으면 만들고 false
         Type uiType = typeof(T);
 
         if (_sceneDict.TryGetValue(uiType, out UI existingUI))
@@ -75,8 +74,8 @@ public class UIManager : Singleton<UIManager>
     }
 
     public T ShowUI<T>(Transform par) where T : UI
-    {
-        if(par == null) return ShowUI<T>();
+    {//부모를 설정해 줘야 할 때 사용
+        if (par == null) return ShowUI<T>();
 
         Type uiType = typeof(T);
 
@@ -93,7 +92,7 @@ public class UIManager : Singleton<UIManager>
     }
 
     public T ShowUI<T>() where T : UI
-    {
+    {//부모 설정하지 않아도 될 때 사용
         return ShowUI<T>(Root);
     }
 
---
+++ Assets/Scripts/Scene/Scene_Main.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Scene_Main : Scene
+{
+    protected override void Init()
+    {
+        base.Init();
+
+        UIManager.Instance.ShowUI<UI_Info>();
+        UIManager.Instance.ShowUI<UI_Status>();
+        UIManager.Instance.ShowUI<UI_Inventory>();
+    }
+}
